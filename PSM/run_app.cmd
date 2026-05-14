444d7ded82ef6671f8b74fdb924110b92c7f3250
reeelax521 <wayberu120@gmail.com>
2026-05-14 10:29:47 +0800

    Delete PSM directory

--- PSM/run_app.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d E:\undergraduate\Web\PSM
-echo Starting Yuqing Mirror...
-echo.
-..\.venv\Scripts\python.exe -m streamlit run app.py --server.address 127.0.0.1 --server.port 8501
-echo.
-echo Streamlit has stopped. Press any key to close this window.
-pause > nul
